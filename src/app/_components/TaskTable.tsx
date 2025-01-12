dd84b99192572d6a66367832d179b498b153a872
Byron Wall <byron@byroni.us>
2025-01-11 21:00:56 -0500

    Formatting in imports

--- src/app/_components/TaskTable.tsx
+++ src/app/_components/TaskTable.tsx
@@ -1,7 +1,9 @@
 "use client";
+
+import { GenericTable } from "./tables/GenericTable";
+
 import type { Task } from "@prisma/client";
 import type { ColumnDef } from "@tanstack/react-table";
-import { GenericTable } from "./tables/GenericTable";
 
 export function TaskTable({ tasks }: { tasks: Task[] }) {
   const columns: ColumnDef<Task>[] = [
